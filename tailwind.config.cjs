01c5c9f1e60a3c04fbbc5cddd23dfa81e3d88fdc
Maxim Tan <maxim.tan@inlume.io>
2024-06-12 23:18:17 +0200

    fixed

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -24,7 +24,7 @@ module.exports = {
       "black": "#0E0004"
     },
     fontFamily: {
-      sans: ['Source Sans Pro', ...defaultTheme.fontFamily.sans],
+      sans: ['Source Sans 3', ...defaultTheme.fontFamily.sans],
     },
   },
   corePlugins: {
